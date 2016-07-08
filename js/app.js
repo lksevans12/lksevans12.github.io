3a9baf72e2ee08431db8e5968202167d23daeaae
Laura Evans <lksevans@resourceammirati.com>
2016-07-08 09:51:21 -0400

    change fonts

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
     $(this).addClass('active');
   })
 
-	var mainbottom = $('.nav-anchor').offset().top + $('.nav-anchor').height();
+	var mainbottom = $('.about-nav-anchor').offset().top + $('.about-nav-anchor').height();
  
 	$(window).on('scroll',function(){
 
